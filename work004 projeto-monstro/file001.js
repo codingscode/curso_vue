d14b0f0113fdcf85a57d27bd1050075f612db6a5
Heronnes Pereira <heronnestg@hotmail.com>
2020-02-03 20:17:51 -0300

    parte 9

--- work004 projeto-monstro/file001.js
+++ work004 projeto-monstro/file001.js
@@ -1,11 +1,13 @@
 new Vue({
     el: '#app',
     data: {
-       vidaJogador: 100,
+       vidaJogador: 0,
        vidaMonstro: 100
     },
     computed: {
-  
+        haResultado() {
+            return this.vidaJogador == 0 || this.vidaMonsto == 0
+        }
     },
     methods: {
   

578d8f1134fe46d7d23d13aaa9762b5966c8c5f4
Heronnes Pereira <heronnestg@hotmail.com>
2020-02-05 19:08:28 -0300

    parte 12

--- work004 projeto-monstro/file001.js
+++ work004 projeto-monstro/file001.js
@@ -15,9 +15,24 @@ new Vue({
             this.executando = true
             this.vidaJogador = 100
             this.vidaMonstro = 100
+        },
+        atacar(especial) {
+            this.dano(7, 12, false)
+        },
+        dano(min, max, especial) {
+            const plus = especial ? 5 : 0
+            const dano = this.aleatorio(min + plus, max + plus)
+            this.vidaJogador = Math.max(this.vidaJogador - dano, 0)
+        }
+        ,
+        aleatorio(min, max) {
+            const valor = Math.random()*(max - min) + min
+            return Math.round(valor)
         }
     },
     watch: {
-  
+        haResultado(valor) {
+            if(valor) this.executando = false
+        }
     }
 })

850850bbd984dabb0035c0c25ebb1b95bccdc59b
Heronnes Pereira <heronnestg@hotmail.com>
2020-02-06 19:34:36 -0300

    parte 15

--- work004 projeto-monstro/file001.js
+++ work004 projeto-monstro/file001.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
        vidaJogador: 100,
        vidaMonstro: 100,
-       executando: false
+       executando: false,
+       logs: []
     },
     computed: {
         haResultado() {
@@ -17,26 +18,31 @@ new Vue({
             this.vidaMonstro = 100
         },
         atacar(especial) {
-            this.dano('vidaJogador',7, 12, false)
-            this.dano('vidaMonstro',5, 10, especial)
+            this.dano('vidaMonstro', 5, 10, especial, 'Jogador', 'Monstro', 'jogador')
+            if(this.vidaJogador > 0) {
+                this.dano('vidaJogador',7, 12, false, 'Monstro', 'Jogador', 'monstro')   
+            }
         },
-        dano(atributo, min, max, especial) {
+        dano(atributo, min, max, especial, fonte, alvo, classe) {
             const plus = especial ? 5 : 0
             const dano = this.aleatorio(min + plus, max + plus)
             this[atributo] = Math.max(this[atributo] - dano, 0)
-        }
-        ,
+            this.registrarLog(`${fonte} atingiu ${alvo} com ${dano}.`, classe)
+        },
         aleatorio(min, max) {
             const valor = Math.random()*(max - min) + min
             return Math.round(valor)
         },
         curar(min, max) {
-            const cura = this.aleatorio(min, max)
-            this.vidaJogador = Math.min(this.vidaJogador + cura, 100)
+            const curar = this.aleatorio(min, max)
+            this.vidaJogador = Math.min(this.vidaJogador + curar, 100)
         },
         curarEdano() {
             this.curar(10, 15)
             this.dano('vidaJogador', 7, 12, false)
+        },
+        registrarLog(texto, classe) {
+            this.logs.unshift({texto, classe})
         }
     },
     watch: {

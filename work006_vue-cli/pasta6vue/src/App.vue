4e460c9505210f62525505c00fb83426d83d5c0d
Heronnes Pereira <heronnestg@hotmail.com>
2020-02-14 16:16:47 -0300

    parte 25

--- work006_vue-cli/pasta6vue/src/App.vue
+++ work006_vue-cli/pasta6vue/src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <div id="app">
+     {{titulo}}
+     <button @click="titulo += '###' ">+</button>
+   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+     data: function() {
+        return {
+           titulo: 'Teste Data usando Vue'
+        }
+     }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+       background-color: darkgreen;
+       color: gold;
+    }
 </style>

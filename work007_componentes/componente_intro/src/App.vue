88b42e3f5e7533d7c4d14a80aa5eb13e0ca2e8bc
Heronnes Pereira <heronnestg@hotmail.com>
2020-02-17 16:32:08 -0300

    parte 29

---
+++ work007_componentes/componente_intro/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        <h1>Contadores</h1>
+        {{contador}}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+          contador: 0
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>

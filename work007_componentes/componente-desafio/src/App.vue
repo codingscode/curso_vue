806a2f4df437fc8076952efebc4385fe1a0b4a1a
Heronnes Pereira <heronnestg@hotmail.com>
2020-02-21 16:48:45 -0300

    parte 33

---
+++ work007_componentes/componente-desafio/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+	<v-app>
+		<v-navigation-drawer app>
+			<v-toolbar flat>
+				<v-list>
+					<v-list-tile>
+						<v-list-tile-title class="title">
+							Menu
+						</v-list-tile-title>
+					</v-list-tile>
+				</v-list>
+			</v-toolbar>
+
+			<v-divider></v-divider>
+
+			<v-list dense class="pt-0">
+				<v-list-tile v-for="item in itensMenu" :key="item.titulo">
+					<v-list-tile-action>
+						<v-icon>{{ item.icon }}</v-icon>
+					</v-list-tile-action>
+					<v-list-tile-content>
+						<v-list-tile-title>{{ item.titulo }}</v-list-tile-title>
+					</v-list-tile-content>
+				</v-list-tile>
+			</v-list>
+		</v-navigation-drawer>
+
+		<v-toolbar app>
+			<v-toolbar-title class="headline text-uppercase">
+				<span>Desafio</span>
+				<span class="font-weight-light">Componente</span>
+			</v-toolbar-title>
+		</v-toolbar>
+
+		<v-content>
+			<v-carousel>
+				<v-carousel-item
+					v-for="(item,i) in fotos"
+					:key="i"
+					:src="item.src"></v-carousel-item>
+			</v-carousel>
+		</v-content>
+
+		<v-footer class="pa-3" app>
+			<v-spacer></v-spacer>
+			<div>Curso Vue &copy; {{ new Date().getFullYear() }}</div>
+		</v-footer>
+	</v-app>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			itensMenu: [
+				{titulo: 'Início', icon: 'dashboard'},
+				{titulo: 'Sobre', icon: 'question_answer'}
+			],
+			fotos: [
+				{src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'},
+				{src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'},
+				{src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'},
+				{src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'}
+			]
+		}
+	}
+}
+</script>

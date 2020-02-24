2266c70d69789e798cddb659861c52c8c5345ba7
Heronnes Pereira <heronnestg@hotmail.com>
2020-02-24 16:15:23 -0300

    parte 40

--- work007_componentes/3comunicacao/src/components/UsuarioInfo.vue
+++ work007_componentes/3comunicacao/src/components/UsuarioInfo.vue
@@ -2,13 +2,24 @@
     <div class="componente">
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
-        <p>Nome do Usuário: <strong>{{nome}}</strong></p>
+        <!-- <p>Nome do Usuário: <strong>{{nomeAlternativo}}</strong></p> -->
+        <p>Nome do Usuário: <strong>{{inverterNome()}}</strong></p>
     </div>
 </template>
 
 <script>
 export default {
-     props: ['nome']
+     props: ['nome'],
+     /*data() {
+        return {
+            nomeAlternativo: this.nome
+        }
+     },*/
+     methods: {
+        inverterNome() {
+            return this.nome.split('').reverse().join('')
+        }
+     }
 }
 </script>
 

3596ff40599726791811805a83a724ef8c8ad209
Heronnes Pereira <heronnestg@hotmail.com>
2020-03-09 17:21:44 -0300

    pasta 9 ------

---
+++ work009_projeto_tarefas/001/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<div id="app">
+		<h1>Tarefas</h1>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+	body {
+		font-family: 'Lato', sans-serif;
+		background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+		color: #FFF;
+	}
+
+	#app {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+
+	#app h1 {
+		margin-bottom: 5px;
+		font-weight: 300;
+		font-size: 3rem;
+	}
+</style>

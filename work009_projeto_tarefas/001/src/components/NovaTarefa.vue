acc52c900353735e4eebd08345a2f0eaf55d6caa
Heronnes Pereira <heronnestg@hotmail.com>
2020-03-11 16:54:03 -0300

    parte 68

--- work009_projeto_tarefas/001/src/components/NovaTarefa.vue
+++ work009_projeto_tarefas/001/src/components/NovaTarefa.vue
@@ -1,7 +1,8 @@
 <template>
      <div class="nova-tarefa">
-          <input v-model="nome" @keydown.enter="adicionar" type="text">
-          <button @click="adicionar">+</button>
+          <input v-model="nome" @keydown.enter="adicionar" type="text" 
+                 class="form-elemento" placeholder="Nova tarefa?">
+          <button @click="adicionar" class="form-elemento">+</button>
      </div>
 </template>
 
@@ -22,5 +23,29 @@ export default {
 </script>
 
 <style>
+      .nova-tarefa {
+        margin: 35px;
+      }
 
+      .form-elemento {
+        outline: none;
+        font-size: 2rem;
+        border: 1px solid #FFF;
+        padding: 5px 10px 8px;
+        color: #FFF;
+      }
+
+      input.form-elemento {
+        width: 500px;
+        background: #FFF2;
+        border-top-left-radius: 8px;
+        border-bottom-left-radius: 8px;
+      }
+
+      button.form-elemento {
+        border-left: none;
+        background-color: #2196F3;
+        border-top-right-radius: 8px;
+        border-bottom-right-radius: 8px;
+      }
 </style>

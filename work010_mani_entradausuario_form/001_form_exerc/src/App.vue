de13fb3781294dfeef3fe75e30f1b5f99527ac7b
Heronnes Pereira <heronnestg@hotmail.com>
2020-03-18 17:20:30 -0300

    parte 75

--- work010_mani_entradausuario_form/001_form_exerc/src/App.vue
+++ work010_mani_entradausuario_form/001_form_exerc/src/App.vue
@@ -5,13 +5,13 @@
 			<form class="painel">
 				<div class="cabecalho">Formulário</div>
 				<Rotulo nome="E-mail">
-					<input type="text" v-model="email">
+					<input type="text" v-model="usuario.email">
 				</Rotulo>
 				<Rotulo nome="Senha">
-					<input type="password">
+					<input type="password" v-model="usuario.senha">
 				</Rotulo>
 				<Rotulo nome="Idade">
-					<input type="number">
+					<input type="number" v-model="usuario.idade">
 				</Rotulo>
 				<Rotulo nome="Mensagem">
 					<textarea name="" cols="30" rows="5"></textarea>
@@ -39,13 +39,13 @@
 			<div class="painel">
 				<div class="cabecalho">Resultado</div>
 				<Rotulo nome="E-mail">
-					<span>{{email}}</span>
+					<span>{{usuario.email}}</span>
 				</Rotulo>
 				<Rotulo nome="Senha">
-					<span>???</span>
+					<span>{{usuario.senha}}</span>
 				</Rotulo>
 				<Rotulo nome="Idade">
-					<span>???</span>
+					<span>{{usuario.idade}}</span>
 				</Rotulo>
 				<Rotulo nome="Mensagem">
 					<span>???</span>
@@ -76,7 +76,11 @@ export default {
 	components: { Rotulo, Escolha },
 	data() {
 		return {
-			email: ''
+			usuario: {
+				email: '',
+				senha: '',
+				idade: 25
+			}
 		}
 	}
 }

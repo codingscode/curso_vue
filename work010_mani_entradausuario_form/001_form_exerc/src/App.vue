503be453606c1eadd4574cc9d81b8960ab81c891
Heronnes Pereira <heronnestg@hotmail.com>
2020-03-23 16:50:45 -0300

    parte 87

--- work010_mani_entradausuario_form/001_form_exerc/src/App.vue
+++ work010_mani_entradausuario_form/001_form_exerc/src/App.vue
@@ -2,7 +2,7 @@
 	<div id="app">
 		<h1>Registrar Reclamação</h1>
 		<div class="conteudo">
-			<form class="painel">
+			<form class="painel" v-if="!enviado">
 				<div class="cabecalho">Formulário</div>
 				<Rotulo nome="E-mail">
 					<input type="text" v-model.lazy.trim="usuario.email">   <!-- só aparece quando clica fora -->
@@ -38,9 +38,9 @@
 					<Escolha v-model="escolha" />
 				</Rotulo>
 				<hr>
-				<button>Enviar</button>
+				<button @click.prevent="enviar">Enviar</button>
 			</form>
-			<div class="painel">
+			<div class="painel" v-else>
 				<div class="cabecalho">Resultado</div>
 				<Rotulo nome="E-mail">
 					<span>{{usuario.email}}</span>
@@ -98,7 +98,8 @@ export default {
 				senha: '',
 				idade: 25
 			},
-			escolha: true
+			escolha: true,
+			enviado: false
 		}
 	},
 	computed: {
@@ -107,6 +108,11 @@ export default {
 		},
 		tipoPrioridade() {
 			return typeof this.prioridade
+		}
+	},
+	methods: {
+		enviar() {
+			this.enviado = true
 		}
 	}
 }

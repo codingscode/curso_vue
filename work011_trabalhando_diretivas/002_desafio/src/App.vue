f3512badb003ccdc4b0a65fa1208a48aa60ae0ce
Heronnes Pereira <heronnestg@hotmail.com>
2020-03-30 12:20:37 -0300

    parte 101

---
+++ work011_trabalhando_diretivas/002_desafio/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+	<div id="app">
+		<h1>Diretivas (Desafio)</h1>
+		<hr>
+		<!-- Exercício -->
+		<!-- Escreva uma diretiva que funcione com o v-on (escute eventos) -->
+	</div>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+
+button {
+	margin: 10px 0px;
+	padding: 10px 20px;
+	font-size: 1.4rem;
+	border-radius: 5px;
+	color: #FFF;
+	background-color: #2196F3;
+}
+</style>

f58af2d5b4da071e6cba7bcc1a6f8c2cea5e7bd0
Heronnes Pereira <heronnestg@hotmail.com>
2020-03-30 11:55:12 -0300

    parte 99

--- work011_trabalhando_diretivas/001/src/App.vue
+++ work011_trabalhando_diretivas/001/src/App.vue
@@ -10,6 +10,7 @@
 		<hr>
 		<p v-destaque-local:fundo.atrasar="'purple'"> Usando diretiva personalizada </p>  <!-- f5 navegador -->
 		<p v-destaque-local="cor"> Usando diretiva personalizada </p>   <!-- experimentar :fundo -->
+		<p v-destaque-local.alternar="cor"> Usando diretiva personalizada </p>   <!-- experimentar :fundo -->
 	</div>
 </template>
 
@@ -19,19 +20,36 @@ export default {
 	  directives: {
 		 'destaque-local': {
               bind(el, binding, vnode) {        //binding e vnode nesse caso podem ser omitidos
-				//el.style.backgroundColor = 'lightgreen'
+				
+                const aplicarCor = cor => {
+                    if (binding.arg === 'fundo') {
+						el.style.backgroundColor = cor
+					}
+					else {
+						el.style.color = cor
+					}
+                }
 
 				let atraso = 0
 				if (binding.modifiers['atrasar']) atraso = 3000
 
+				
+
+                const cor1 = binding.value
+				const cor2 = 'blue'
+				let corAtual = cor1
+ 
 				setTimeout(() => {
-					if (binding.arg === 'fundo') {
-						el.style.backgroundColor = binding.value
-					}
+                    if (binding.modifiers['alternar']) {
+                         setInterval(() => {
+							 corAtual = corAtual === cor1 ? cor2 : cor1
+                             aplicarCor(corAtual)
+						 }, 1000)
+                    }
 					else {
-						el.style.color = binding.value
-					}
-				}, atraso)
+                        aplicarCor(binding.value)
+					} 
+                }, atraso)
 				
 			 }
 	     }

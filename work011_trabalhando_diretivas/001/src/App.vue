50db13d4fa41c31a55200c9dff3146766487fb5f
Heronnes Pereira <heronnestg@hotmail.com>
2020-03-30 11:24:21 -0300

    parte 98

--- work011_trabalhando_diretivas/001/src/App.vue
+++ work011_trabalhando_diretivas/001/src/App.vue
@@ -3,15 +3,39 @@
 		<h1>Diretivas</h1>
 		<hr>
 		<p v-text="'Usando diretiva v-text'"></p> <!-- experimentar usar 'Usando diretiva <strong>v-text</html>'  -->
-		<p v-html="'Usando diretiva <strong>v-html</html>'"></p>
+		<p v-html="'Usando diretiva <strong>v-html</strong>'"></p>
 		<hr>
 		<p v-destaque:fundo.atrasar="'orange'"> Usando diretiva personalizada </p>  <!-- f5 navegador -->
 		<p v-destaque="cor"> Usando diretiva personalizada </p>   <!-- experimentar :fundo -->
+		<hr>
+		<p v-destaque-local:fundo.atrasar="'purple'"> Usando diretiva personalizada </p>  <!-- f5 navegador -->
+		<p v-destaque-local="cor"> Usando diretiva personalizada </p>   <!-- experimentar :fundo -->
 	</div>
 </template>
 
 <script>
 export default {
+	  components: {},
+	  directives: {
+		 'destaque-local': {
+              bind(el, binding, vnode) {        //binding e vnode nesse caso podem ser omitidos
+				//el.style.backgroundColor = 'lightgreen'
+
+				let atraso = 0
+				if (binding.modifiers['atrasar']) atraso = 3000
+
+				setTimeout(() => {
+					if (binding.arg === 'fundo') {
+						el.style.backgroundColor = binding.value
+					}
+					else {
+						el.style.color = binding.value
+					}
+				}, atraso)
+				
+			 }
+	     }
+	  },
 	  data() {
 		  return {
 			  cor: 'green'

3ad33673fcc8dd2ef69cb711042b691fd9bc4b7b
Heronnes Pereira <heronnestg@hotmail.com>
2020-04-03 17:09:31 -0300

    parte 118

---
+++ work012_filtros_mixins/002_desafio/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+	<div id="app">
+		<h1>Filtros & Mixins (Desafio)</h1>
+		<!-- Exercício 1 -->
+		<!-- Construir um filtro local que troca espaços por vírgula -->
+		
+		<!-- Exercício 2 -->
+		<!-- Filtro global que conta o tamanho de cada palavra e adiciona o 
+			valor na string final -->
+		<!-- "Pedro é legal" => "Pedro (5) é (1) legal (5)" -->
+
+		<!-- Exercício 3 -->
+		<!-- Implementar os exercicios 1 e 2 com propriedade computada -->
+
+		<!-- Exercício 4 -->
+		<!-- Compartilhe a propriedade computada via mixin -->
+	</div>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+</style>

2a98695ce7262fa926cca64c6a79d5f36749b4b5
Heronnes Pereira <heronnestg@hotmail.com>
2020-04-08 20:21:13 -0300

    parte 124

--- work013_adicionando_animacoes_transicoes/001/src/App.vue
+++ work013_adicionando_animacoes_transicoes/001/src/App.vue
@@ -3,7 +3,10 @@
 		<h1>Animações</h1>
 		<hr>
 		<b-button variant="primary" class="mb-4" @click="exibir = !exibir">Mostrar Mensagem</b-button>
-		<b-alert variant="info" show v-if="exibir">{{msg}}</b-alert>
+		<transition name="fade">   <!-- transition sozinho n gera efeito algum -->
+             <b-alert variant="info" show v-if="exibir">{{msg}}</b-alert>
+		</transition>
+		
 	</div>
 </template>
 
@@ -28,5 +31,29 @@ export default {
 	color: #2c3e50;
 	margin-top: 60px;
 	font-size: 1.5rem;
+}
+
+.fade-enter {
+
+}
+
+.fade-enter-active {
+
+}
+
+.fade-enter-to {
+
+}
+
+.fade-leave {
+
+}
+
+.fade-leave-active {
+
+}
+
+.fade-leave-to {
+
 }
 </style>

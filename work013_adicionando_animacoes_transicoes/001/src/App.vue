7ba7416be015e15eb19cf474e35462f071387b45
Heronnes Pereira <heronnestg@hotmail.com>
2020-04-08 12:56:45 -0300

    ------- pasta 14 ----

---
+++ work013_adicionando_animacoes_transicoes/001/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="app" class="container-fluid">
+		<h1>Animações</h1>
+	</div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 1.5rem;
+}
+</style>

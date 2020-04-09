d17a9d1242886ad310f8066929f68167d5a7944d
Heronnes Pereira <heronnestg@hotmail.com>
2020-04-08 21:00:08 -0300

    parte 127

--- work013_adicionando_animacoes_transicoes/001/src/App.vue
+++ work013_adicionando_animacoes_transicoes/001/src/App.vue
@@ -7,6 +7,10 @@
              <b-alert variant="info" show v-if="exibir">{{msg}}</b-alert>
 		</transition>
 		
+		<transition name="slide">   
+             <b-alert variant="info" show v-if="exibir">{{msg}}</b-alert>
+		</transition>
+		
 	</div>
 </template>
 
@@ -41,4 +45,21 @@ export default {
      transition: opacity 2s;
 }
 
+@keyframes slide-in {
+   from {transform: translateY(40px);}
+   to {transform: translateY(0);}
+}
+
+@keyframes slide-out {
+   from {transform: translateY(0);}
+   to {transform: translateY(40px);}
+}
+
+.slide-enter-active {
+   animation: slide-in 2s ease;
+}
+
+.slide-leave-active {
+   animation: slide-out 2s ease;
+}
 </style>

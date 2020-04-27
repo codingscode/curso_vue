f9383b5cd0b43bdace6f9388bde0df9accf81661
Heronnes Pereira <heronnestg@hotmail.com>
2020-04-27 12:52:37 -0300

    parte 158

--- work014_conectando-via-servidor-http/001_http-exercicios/src/App.vue
+++ work014_conectando-via-servidor-http/001_http-exercicios/src/App.vue
@@ -1,19 +1,42 @@
 <template>
 	<div id="app" class="container">
 		<h1>HTTP com Axios</h1>
+		<b-card>
+			<b-form-group label="Nome:">
+                <b-form-input type="text" size="lg" v-model="usuario.nome" placeholder="Informe o Nome"></b-form-input>
+			</b-form-group>
+			<b-form-group label="E-mail:">
+                <b-form-input type="email" size="lg" v-model="usuario.email" placeholder="Informe o E-mail"></b-form-input>
+			</b-form-group>
+			<hr>
+			<b-button @click="salvar" size="lg" variant="primary">Salvar</b-button>
+		</b-card>
 	</div>
 </template>
 
 <script>
 export default {
-     created() {
+	 data() {
+		return {
+			usuario: {
+				nome: '',
+				email: ''
+			}
+		}
+	 },
+	 methods: {
+		salvar() {
+		   console.log(this.usuario)   // ver console
+		}
+	 }
+     /*created() {
 		 console.log('executando...')
 		 this.$http.post('usuarios.json', {      // verificar firebase e console
 				nome: 'Fulanis',
 				email: 'fulanis_fulanis@gmail.com'
 		  }).then(res => console.log(res))
 
-	 }
+	 }*/
 }
 </script>
 

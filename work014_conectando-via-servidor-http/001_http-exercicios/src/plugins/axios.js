c223f422464f4e4db9c6abb9a0ce78c2f39c6594
Heronnes Pereira <heronnestg@hotmail.com>
2020-04-27 12:14:28 -0300

    parte 157

--- work014_conectando-via-servidor-http/001_http-exercicios/src/plugins/axios.js
+++ work014_conectando-via-servidor-http/001_http-exercicios/src/plugins/axios.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-axios.defaults.baseURL= 'https://curso-vue-25e73.firebaseio.com/'
+//axios.defaults.baseURL= 'https://curso-vue-25e73.firebaseio.com/'
 
 Vue.use({
     install(Vue) {
-       Vue.prototype.$http = axios
+       //Vue.prototype.$http = axios
+       Vue.prototype.$http = axios.create({
+           baseURL: 'https://curso-vue-25e73.firebaseio.com/'
+       })
     }
 })

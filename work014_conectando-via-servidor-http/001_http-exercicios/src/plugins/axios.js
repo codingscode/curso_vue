ff554224ee3a3ed4451e9c3715cfdc646acea8cf
Heronnes Pereira <heronnestg@hotmail.com>
2020-04-29 20:27:56 -0300

    parte 165

--- work014_conectando-via-servidor-http/001_http-exercicios/src/plugins/axios.js
+++ work014_conectando-via-servidor-http/001_http-exercicios/src/plugins/axios.js
@@ -11,10 +11,10 @@ Vue.use({
        })
 
        Vue.prototype.$http.interceptors.request.use(config => {
-            
+            /*
             if (config.method == 'post') {
                config.method = 'put'
-            }
+            }*/
             console.log('config aê: ' + config.method)
             return config
             

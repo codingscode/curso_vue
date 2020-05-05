0fd621421df6380d677b82c6738d5fe482fbb594
Heronnes Pereira <heronnestg@hotmail.com>
2020-05-04 21:22:14 -0300

    --------------------

--- work014_conectando-via-servidor-http/001_http-exercicios/src/App.vue
+++ work014_conectando-via-servidor-http/001_http-exercicios/src/App.vue
@@ -76,7 +76,7 @@ export default {
 			this.usuario = {...this.usuarios[id]}
 		},
 		excluir(id) {
-			this.$http.delete(`/usuarios/${id}`)
+			this.$http.delete(`/usuarios/${id}.json`)
 				.then(() => this.limpar())
 				.catch(err => {
 					this.mensagens.push({
---
+++ work015_vue_rotas/001/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="app">
+		<h1>Rotas com VueRouter</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style>
+	#app {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>

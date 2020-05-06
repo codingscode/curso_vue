e86b868cc0261363cf84746c51e92a5e10278ba1
Heronnes Pereira <heronnestg@hotmail.com>
2020-05-06 18:50:40 -0300

    parte 180

--- work015_vue_rotas/001/src/App.vue
+++ work015_vue_rotas/001/src/App.vue
@@ -1,13 +1,16 @@
 <template>
 	<div id="app">
 		<h1>Rotas com VueRouter</h1>
+		<Menu />
 		<router-view />
 	</div>
 </template>
 
 <script>
+import Menu from './components/template/Menu'
+
 export default {
-	
+	components: {Menu}
 }
 </script>
 
---
+++ work015_vue_rotas/001/src/components/template/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav class="menu">
+        <router-link to="/">Início</router-link>
+        <router-link to="/usuario">Usuário</router-link>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

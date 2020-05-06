35e9d5d3561e9c09e64f4552d3e5b9540470feb3
Heronnes Pereira <heronnestg@hotmail.com>
2020-05-06 19:01:36 -0300

    parte 181

--- work015_vue_rotas/001/src/components/template/Menu.vue
+++ work015_vue_rotas/001/src/components/template/Menu.vue
@@ -1,7 +1,14 @@
 <template>
     <nav class="menu">
-        <router-link to="/">Início</router-link>
-        <router-link to="/usuario">Usuário</router-link>
+        <ul>
+            <router-link to="/" tag="li" active-class="active" exact>
+                 <a>Início</a>
+            </router-link>
+            <router-link to="/usuario" tag="li" active-class="active">
+                 <a>Usuário</a>
+            </router-link>
+        </ul>
+        
     </nav>
 </template>
 

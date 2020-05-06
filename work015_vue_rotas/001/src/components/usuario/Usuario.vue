5b26c0d07d6b890a527bfc31a55ade3feeff4c9a
Heronnes Pereira <heronnestg@hotmail.com>
2020-05-06 19:34:11 -0300

    parte 184

--- work015_vue_rotas/001/src/components/usuario/Usuario.vue
+++ work015_vue_rotas/001/src/components/usuario/Usuario.vue
@@ -2,12 +2,18 @@
     <div class="usuario">
         <h2>Usuários</h2>
         <hr>
+        <p><strong>Código:</strong> {{id}}</p>
         <button sucesso @click="irParaInicio">Voltar</button>
     </div>
 </template>
 
 <script>
 export default {
+     data() {
+        return {
+            id: this.$route.params.id
+        }
+     },    
      methods: {
         irParaInicio() {
             //this.$router.push('/') 

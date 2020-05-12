3798cba9dca30d5d1162ea5a5594cbaffbb1d581
Heronnes Pereira <heronnestg@hotmail.com>
2020-05-11 22:09:33 -0300

    parte 196

--- work015_vue_rotas/001/src/components/usuario/UsuarioEditar.vue
+++ work015_vue_rotas/001/src/components/usuario/UsuarioEditar.vue
@@ -2,7 +2,8 @@
     <div class="usuario-editar">
         <h3>Usuário Editar</h3>
         <p><strong>Código:</strong> {{id}} || {{$route.params.id}}</p>
-        
+        <p><strong>Completo:</strong> {{$route.query.completo ? 'Sim' : 'Não'}}</p>
+        <p><strong>Língua:</strong> {{$route.query.lingua}}</p>
     </div>
 </template>
 

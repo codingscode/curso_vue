1c42547f9e7d5b29181019f137ffb0009e679adb
Heronnes Pereira <heronnestg@hotmail.com>
2020-05-13 12:43:38 -0300

    parte 203

--- work015_vue_rotas/001/src/App.vue
+++ work015_vue_rotas/001/src/App.vue
@@ -2,7 +2,7 @@
 	<div id="app">
 		<h1>Rotas com VueRouter</h1>
 		<router-view name="menu"></router-view>
-		<transition enter-active-class="animated rubberBand" leave-active-class="animated rollOut">
+		<transition mode="out-in" enter-active-class="animated rubberBand" leave-active-class="animated rollOut">
             <router-view />
 		</transition>
 		<router-view name="menuInferior"></router-view>

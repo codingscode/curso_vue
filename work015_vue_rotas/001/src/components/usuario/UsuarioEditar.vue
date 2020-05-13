14b11dd8607306284e0eaf19a5a3b16db41aecf0
Heronnes Pereira <heronnestg@hotmail.com>
2020-05-13 12:55:01 -0300

    parte 204

--- work015_vue_rotas/001/src/components/usuario/UsuarioEditar.vue
+++ work015_vue_rotas/001/src/components/usuario/UsuarioEditar.vue
@@ -4,6 +4,9 @@
         <p><strong>Código:</strong> {{id}} || {{$route.params.id}}</p>
         <p><strong>Completo:</strong> {{$route.query.completo ? 'Sim' : 'Não'}}</p>
         <p><strong>Língua:</strong> {{$route.query.lingua}}</p>
+        <div id="rodape">
+           <h3>Curso Vue</h3>
+        </div>
     </div>
 </template>
 
@@ -14,5 +17,11 @@ export default {
 </script>
 
 <style>
-
+    #rodape {
+       margin-top: 1000px;
+    }
 </style>
+<!--
+  testar http://localhost:8080/usuario/2/editar?completo=true&lingua=pt
+         http://localhost:8080/usuario/2/editar?completo=true&lingua=pt#rodape
+-->

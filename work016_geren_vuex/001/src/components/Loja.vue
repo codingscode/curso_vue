a14b5368ccefee1b549b172bb3f181c6189a43ea
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-05 21:07:16 -0300

    parte 232

--- work016_geren_vuex/001/src/components/Loja.vue
+++ work016_geren_vuex/001/src/components/Loja.vue
@@ -11,6 +11,7 @@
 </template>
 
 <script>
+import {mapMutations} from 'vuex'
 
 export default {
     data() {
@@ -21,6 +22,7 @@ export default {
         }
     },
     methods: {
+        ...mapMutations(['adicionarProduto']),
         adicionar() {
             const produto = {
                 id: this.sequencia,
@@ -32,7 +34,8 @@ export default {
             
             //this.$store.state.produtos.push(produto)
 
-            this.$store.commit('adicionarProduto', produto)
+            //this.$store.commit('adicionarProduto', produto)
+            this.adicionarProduto(produto)
             // eslint-disable-next-line
             console.log(produto)
         }

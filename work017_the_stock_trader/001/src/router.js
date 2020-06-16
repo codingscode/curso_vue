6e26dc90b4bde0ad791d919aefb868a87101a21b
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-15 22:17:34 -0300

    parte 247

--- work017_the_stock_trader/001/src/router.js
+++ work017_the_stock_trader/001/src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Home from './components/Home'
+import Inicio from './components/Inicio'
 import Portfolio from './components/portfolio/Portfolio'
 import Stocks from './components/stocks/Stocks'
 
@@ -10,7 +10,7 @@ Vue.use(Router)
 export default new Router({
     mode: 'history',
     routes: [
-        { path: '/', component: Home },
+        { path: '/', component: Inicio },
         { path: '/portfolio', component: Portfolio },
         { path: '/stocks', component: Stocks },
     ]

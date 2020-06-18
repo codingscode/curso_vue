df82ddac7982e32ae486280b1856dd5dd6c2854e
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-17 21:09:16 -0300

    parte 250

--- work017_the_stock_trader/001/src/components/stocks/Stock2.vue
+++ work017_the_stock_trader/001/src/components/stocks/Stock2.vue
@@ -1,7 +1,17 @@
 <template>
-    <div>
-        <h1>Ação</h1>
-    </div>
+    <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+       <v-card class="green darken-3 white--text">
+          <v-card-title class="headline">
+              <strong>NOME <small>(Preço: PRECO)</small></strong>
+          </v-card-title>
+       </v-card>
+       <v-card>
+           <v-container fill-height>
+               <v-text-field label="Quantidade" type="number"/>
+               <v-btn class="green darken-3 white--text">Comprar</v-btn>
+           </v-container>
+       </v-card>
+    </v-flex>
 </template>
 
 <script>
--- work017_the_stock_trader/001/src/components/stocks/Stocks2.vue
+++ work017_the_stock_trader/001/src/components/stocks/Stocks2.vue
@@ -1,12 +1,24 @@
 <template>
-     <div>
-         <h1>Ações</h1>
-     </div>
+     <v-layout row wrap>
+         <Stock2 v-for="stock in stocks" :key="stock.id"/>
+     </v-layout>
 </template>
 
 <script>
-export default {
+import Stock2 from './Stock2'
 
+export default {
+     components: {Stock2},
+     data() {
+         return {
+            stocks: [
+                {id: 1, nome: 'BMW', preco: 110},
+                {id: 2, nome: 'Google', preco: 200},
+                {id: 3, nome: 'Apple', preco: 280},
+                {id: 4, nome: 'Twitter', preco: 83}
+            ]
+        }
+     }
 }
 </script>
 

5d4ac5eba284dbe2c4c8d33a825027bb1fd70585
Heronnes Pereira <heronnestg@hotmail.com>
2020-07-01 21:42:58 -0300

    parte 268

--- work017_the_stock_trader/001/src/App.vue
+++ work017_the_stock_trader/001/src/App.vue
@@ -3,7 +3,10 @@
 		<Cabecalho />
 		<v-content>
 			<v-container>
-				<router-view></router-view>
+				<transition name="slide" mode="out-in">
+                    <router-view></router-view>
+				</transition>
+				
 			</v-container>
 		</v-content>
 	</v-app>
@@ -22,6 +25,36 @@ export default {
 </script>
 
 <style>
+     @keyframes slide-in {
+		from {
+			transform: translateY(-30px);
+			opacity: 0;
+		}
 
+		to {
+			transform: translateY(0px);
+			opacity: 1;
+		}
+	 }
+
+	 @keyframes slide-out {
+		from {
+			transform: translateY(0px);
+			opacity: 1;
+		}
+
+		to {
+			transform: translateY(-30px);
+			opacity: 0;
+		}
+	 }
+
+	 .slide-enter-active {
+		animation: slide-in 0.3s ease;
+	 }
+
+	  .slide-leave-active {
+		animation: slide-out 0.3s ease;
+	  }
 </style>
 

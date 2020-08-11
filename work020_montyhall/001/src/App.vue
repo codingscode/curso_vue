6d4f117b7ae561adb1f81db71bf931b4fd235c44
Heronnes Pereira <heronnestg@hotmail.com>
2020-08-10 21:43:49 -0300

    parte 281

--- work020_montyhall/001/src/App.vue
+++ work020_montyhall/001/src/App.vue
@@ -1,6 +1,9 @@
 <template>
-      <div>
-      		Oi Vue
+      <div id="app">
+		    <h1>
+      			Oi Vue
+            </h1>
+			
       </div>
 </template>
 
@@ -11,5 +14,26 @@ export default {
 </script>
 
 <style>
+		* {
+			box-sizing: border-box;
+			font-family: 'Arial';
+		}
 
+		body {
+			color: #FFF;
+			background: linear-gradient(to right, rgb(36, 121, 3), rgb(59, 201, 2))
+		}
+
+		#app {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+		}
+
+		#app h1 {
+			border: 1px solid #000;
+			background-color: #0004;
+			padding: 20px;
+			margin-bottom: 60px;
+		}
 </style>
